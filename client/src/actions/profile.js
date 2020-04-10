2d6c2a1f215e438be5f67ce67eb6108321fc5dad
CUAN O CONCHUIR DILLON <G00300230@gmit.ie>
2020-04-10 19:05:16 +0100

    Displaying profiles on dashboard from Database, and fixed some console warnings

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -256,7 +256,7 @@ export const deleteAccount = () => async dispatch => {
     {
         try 
         {
-            const res = await axios.delete('/api/profile');
+            await axios.delete('./api/profile');
             dispatch({ type: CLEAR_PROFILE });
             dispatch({ type: ACCOUNT_DELETED });
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,15 +9,23 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-      <li><Link to="/dashboard">
-      <i className="fas fa-user"></i>{' '}
-      <span className= 'hide-sm'>Dashboard</span></Link></li>
+      <li>
+        <Link to="/profiles">
+          Developers
+        </Link>
+      </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{' '}
+          <span className= 'hide-sm'>Dashboard</span>
+        </Link>
+      </li>
     <li>
       <a onClick={logout} href='#!'>
         <i className="fas fa-sign-out-alt"></i>{' '}
         <span className= 'hide-sm'>Logout</span>
-        </a>
-      </li>
+      </a>
+    </li>
    
   </ul>
 
@@ -25,7 +33,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const guestLinks = (
   <ul>
-    <li><Link to="/profiles">Developers</Link></li>
+     <li>
+        <Link to="/profiles">
+          Developers
+        </Link>
+      </li>
     <li><Link to="/register">Register</Link></li>
     <li><Link to="/login">Login</Link></li>
   </ul>);
